81c94fe0afa11adf6cd631e1f3f264c4db9e7763
Timo Gronau <timogronau@gmail.com>
2023-09-05 10:47:48 +0200

    first view

--- apps/resources/views.py
+++ apps/resources/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import TemplateView
 
-# Create your views here.
+def home_page(request):
+    response = "<html><h1>Welcome to ResourceShare</1></html>"
+    return HttpResponse(response)
+
+class HomePage(TemplateView):
+    template_name = 'home_page.html'
